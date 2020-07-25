cb88b3e4cd3ee80e176df2b02bdc45bd01c38df0
Simeon Fadahunsi <adedayosimeon@gmail.com>
2020-07-25 14:46:33 +0100

    test commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    
+    extend: {
+      colors: {
+        purple: '#601f69',
+      },
+      lineHeight: {
+              'extra-loose': '3.6',
+            
+     },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
